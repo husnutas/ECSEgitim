a9d2ed8626725256ae2d91343f3b4af20505a0c3
husnutas <husnu@ecodation.com>
2024-03-19 21:23:08 +0300

    Initial Commit

---
+++ ECSTestSUI/ECSTestSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECSTestSUIApp.swift
+//  ECSTestSUI
+//
+//  Created by Hüsnü Taş on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ECSTestSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
